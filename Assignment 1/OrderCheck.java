1ab80166a5d685b9c96c00a31dcfac0c2b1088c6
jqyanes <yanes.janine@gmail.com>
2024-07-02 11:11:44 -0400

    Update OrderCheck.java

--- Assignment 1/OrderCheck.java
+++ Assignment 1/OrderCheck.java
@@ -2,7 +2,7 @@
  *  Compilation:  javac OrderCheck.java
  *  Execution:    java OrderCheck 5 10 15 2
  *
- *  @author: Janine Yanes, jqy2@scarletmail.rutgers.edu, jqy2
+ *  @author: Janine Yanes
  *
  *  Prints true if the four integer inputs are in strictly ascending
  *  or descending order, false otherwise
@@ -23,7 +23,7 @@ public class OrderCheck {
 
         int w = Integer.parseInt(args[0]);
         int x = Integer.parseInt(args[1]);
-	    int y = Integer.parseInt(args[2]);
+	int y = Integer.parseInt(args[2]);
         int z = Integer.parseInt(args[3]);
         boolean inOrder; 
         inOrder = (w < x) && (x < y) && (y < z);
