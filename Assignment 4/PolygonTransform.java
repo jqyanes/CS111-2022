16d74e8fb638612c75a3d6f491e48c1eb98251af
jqyanes <yanes.janine@gmail.com>
2024-07-02 12:48:24 -0400

    Update PolygonTransform.java

--- Assignment 4/PolygonTransform.java
+++ Assignment 4/PolygonTransform.java
@@ -14,28 +14,28 @@ public class PolygonTransform {
         double[] a = new double[array.length];
         for (int i = 0; i < array.length; i++) a[i] = array[i];
         return a;
-        }
+    }
         
-        // Scales the given polygon by the factor alpha. 
-        public static void scale(double[] x, double[] y, double alpha) {
-      for (int i = 0; i < x.length; i++) x[i] = (x[i] * alpha);
+    // Scales the given polygon by the factor alpha. 
+    public static void scale(double[] x, double[] y, double alpha) {
+        for (int i = 0; i < x.length; i++) x[i] = (x[i] * alpha);
         for (int i = 0; i < y.length; i++) y[i] = (y[i] * alpha);
-        }
+    }
         
-        // Translates the given polygon by (dx, dy). 
-        public static void translate(double[] x, double[] y, double dx, double dy) {
+     // Translates the given polygon by (dx, dy). 
+    public static void translate(double[] x, double[] y, double dx, double dy) {
         for (int i = 0; i < x.length; i++) x[i] = (x[i] + dx);
         for (int i = 0; i < y.length; i++) y[i] = (y[i] + dy);
-        }
+    }
         
-        // Rotates the given polygon theta degrees counterclockwise, about the origin. 
-        public static void rotate(double[] x, double[] y, double theta) {
+    // Rotates the given polygon theta degrees counterclockwise, about the origin. 
+    public static void rotate(double[] x, double[] y, double theta) {
         double thetaRadians = ((theta / 360) * (2 * Math.PI));
         double[] x2 = new double[x.length];
         for (int i = 0; i < x.length; i++) x2[i] = x[i];
         for (int i = 0; i < x.length; i++) x[i] = ((x[i] * Math.cos(thetaRadians)) - (y[i] * Math.sin(thetaRadians)));
         for (int i = 0; i < y.length; i++) y[i] = ((y[i] * Math.cos(thetaRadians)) + (x2[i] * Math.sin(thetaRadians)));
-        }
+    }
 
     // Tests each of the API methods by directly calling them. 
     public static void main(String[] args) {
